a4fe7ef06625f7c1b7e0d9ccd3be1b79b194a734
Iluze25 <153940117+Iluze25@users.noreply.github.com>
2024-06-23 23:55:23 +0700

    Revisi 1
    Signed-off-by: Iluze25 <153940117+Iluze25@users.noreply.github.com>

---
+++ Web Ujian online -Semester 1/script.js
@@ -0,0 +1,16 @@
+function materi() {
+    window.location.href = "/materi/materi.html";
+}
+
+function loginValue() {
+    const username = document.getElementById('username').value.toLowerCase();
+    const password = document.getElementById('password').value
+
+    if (username === "indonesia" && password === "Tasikmalaya") {
+        alert('Anda akan segera masuk ke Materi');
+        materi();
+    } else {
+        const style = document.getElementById('peringatan');
+        style.style.display = "block";
+    }
+}
