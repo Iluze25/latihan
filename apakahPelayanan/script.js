6e87663847f645597e04d4ae9a784b71bfaa3c6c
Iluze Ear <nsumarna150@gmail.com>
2024-07-21 02:38:53 +0700

    Pelayanan

---
+++ apakahPelayanan/script.js
@@ -0,0 +1,14 @@
+const Person = [
+    "Aditya",
+    "Ardinsyah",
+    "20",
+    "160 cm",
+    Person[0] + Person[1]
+]
+
+function ASD() {
+    document.getElementById('tombol').style.top = Math.floor(Math.random() * 150) + "px"
+    document.getElementById('tombol').style.left = Math.floor(Math.random() * 60) + "px"
+
+    
+}
